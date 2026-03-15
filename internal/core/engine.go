8b3e5c7b0b3e5a81d362810ffdd5ea939ec192e8
Michael Martin <michaeljmartin28@gmail.com>
2026-03-15 01:37:22 -0400

    Refactor Engine methods to return pointers for response types

--- internal/core/engine.go
+++ internal/core/engine.go
@@ -16,29 +16,29 @@ func NewEngine(store KeyStore) *Engine {
 // compile-time assertion to ensure Engine implements the CoreKMS interface
 var _ CoreKMS = (*Engine)(nil)
 
-func (e *Engine) CreateKey(ctx context.Context, req CreateKeyRequest) (CreateKeyResponse, error) {
+func (e *Engine) CreateKey(ctx context.Context, req CreateKeyRequest) (*CreateKeyResponse, error) {
 	// TODO: implement
-	return CreateKeyResponse{}, nil
+	return &CreateKeyResponse{}, nil
 }
 
-func (e *Engine) Encrypt(ctx context.Context, req EncryptRequest) (EncryptResponse, error) {
+func (e *Engine) Encrypt(ctx context.Context, req EncryptRequest) (*EncryptResponse, error) {
 	// TODO: implement
-	return EncryptResponse{}, nil
+	return &EncryptResponse{}, nil
 }
 
-func (e *Engine) Decrypt(ctx context.Context, req DecryptRequest) (DecryptResponse, error) {
+func (e *Engine) Decrypt(ctx context.Context, req DecryptRequest) (*DecryptResponse, error) {
 	// TODO: implement
-	return DecryptResponse{}, nil
+	return &DecryptResponse{}, nil
 }
 
-func (e *Engine) GenerateDataKey(ctx context.Context, req GenerateDataKeyRequest) (GenerateDataKeyResponse, error) {
+func (e *Engine) GenerateDataKey(ctx context.Context, req GenerateDataKeyRequest) (*GenerateDataKeyResponse, error) {
 	// TODO: implement
-	return GenerateDataKeyResponse{}, nil
+	return &GenerateDataKeyResponse{}, nil
 }
 
-func (e *Engine) DecryptDataKey(ctx context.Context, req DecryptDataKeyRequest) (DecryptDataKeyResponse, error) {
+func (e *Engine) DecryptDataKey(ctx context.Context, req DecryptDataKeyRequest) (*DecryptDataKeyResponse, error) {
 	// TODO: implement
-	return DecryptDataKeyResponse{}, nil
+	return &DecryptDataKeyResponse{}, nil
 }
 
 func (e *Engine) RotateKey(ctx context.Context, keyID string) (int, error) {
